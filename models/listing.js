6a534419c9e6ed09320ba95d2f422096a9c47d9c
prasad9056 <jatlaprasad477@gmail.com>
2026-09-10 19:57:02 +0530

    Updated project files

--- models/listing.js
+++ models/listing.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const schema= mongoose.Schema;
+const Review=require('./review.js');
 const listingschema = new schema({
     title:{
         type: String,
@@ -27,6 +28,10 @@ const listingschema = new schema({
       },
     ],
 });
-
+listingschema.post("findOneAndDelete", async  (listing)=>{
+    if(listing){
+        await Review.deleteMany({_id:{$in:listing.reviews}});
+    }
+});
 const listing= mongoose.model('listing',listingschema);
 module.exports=listing;
